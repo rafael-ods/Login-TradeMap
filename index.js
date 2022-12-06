b89e401b6781c3efa1185d43a8fcfde9cd3ef7a6
Rafael Oliveira <rafaeloliveirafinanca@gmail.com>
2022-12-06 18:07:59 -0300

    getinfoforms

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-window.addEventListener('load', (event) => {
-    function toggleButton () {
-    
-        if (username && password) {
-            document.querySelector("#login").disabled = false
-            return
-        }
-    
-        document.querySelector("#login").disabled = true
-    }
- });
- 
+function meuEscopo () {
+
+    const fomrulario = document.querySelector('.fomulario')
+
+    const user = [];
+
+    const receberFormulario = (event) => {
+        event.preventDefault();
+
+        const username = document.getElementById('username')
+        const password = document.getElementById('password')
+
+        user.push({
+            username: username.value,
+            password: password.value,
+        })
+
+        console.log(user)
+    };
+
+
+    fomrulario.addEventListener('submit', receberFormulario)
+
+};
+
+meuEscopo();
+
+
+
